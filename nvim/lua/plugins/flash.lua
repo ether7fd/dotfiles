eb71918510971f86ae4a39f832c29c5d6cc43a2b
yuxa yuha <y1uxa24@gmail.com>
2026-05-15 02:13:36 +0900

    chore(nvim): update flash.nvim config

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,47 +1,24 @@
 return {
-	"folke/flash.nvim",
-	event = "VeryLazy",
-	opts = {},
-	keys = {
-		{
-			"s",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").jump()
-			end,
-			desc = "Flash",
-		},
-		{
-			"S",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").treesitter()
-			end,
-			desc = "Flash Treesitter",
-		},
-		{
-			"r",
-			mode = "o",
-			function()
-				require("flash").remote()
-			end,
-			desc = "Remote Flash",
-		},
-		{
-			"R",
-			mode = { "o", "x" },
-			function()
-				require("flash").treesitter_search()
-			end,
-			desc = "Treesitter Search",
-		},
-		{
-			"<c-s>",
-			mode = { "c" },
-			function()
-				require("flash").toggle()
-			end,
-			desc = "Toggle Flash Search",
-		},
-	},
+  "folke/flash.nvim",
+  event = "VeryLazy",
+
+  opts = {
+    modes = {
+      char = {
+        -- f を打った時のターゲットを「現在の行のみ」に制限する（Vim標準と同じ感覚にする）
+        multi_line = false,
+
+        -- f の後に文字を打った時、すぐにラベルを表示してワープできるようにする
+        jump_labels = true,
+      }
+    }
+  },
+
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
 }
