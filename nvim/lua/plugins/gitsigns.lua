cdefcb41b0bfc983362d2b60daaa9efc2003b917
ether <ether7fd@gmail.com>
2026-07-09 21:12:38 +0900

    feat(nvim): add git operation keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -41,6 +41,12 @@ return {
 				gs.blame_line({ full = true })
 			end)
 			map("n", "<leader>gd", gs.toggle_deleted)
+
+			-- 現在の変更箇所の変更・削除を取り消して「元の状態」に復活させる (Reset Hunk)
+			map("n", "<leader>gr", gs.reset_hunk, { desc = "Reset hunk" })
+
+			-- 現在の変更箇所だけを `git add` する (Stage Hunk)
+			map("n", "<leader>gs", gs.stage_hunk, { desc = "Stage hunk" })
 		end,
 	},
 }
