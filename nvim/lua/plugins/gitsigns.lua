67552256c365691606bd61f66603d524e2c5f6a3
ether <ether7fd@gmail.com>
2026-07-10 18:26:39 +0900

    feat(nvim): add git operation on visual mode

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -25,7 +25,7 @@ return {
 					gs.prev_hunk()
 				end)
 				return "<Ignore>"
-			end, { expr = true })
+			end, { expr = true, desc = "Previous Git hunk" })
 
 			map("n", "]g", function()
 				if vim.wo.diff then
@@ -35,7 +35,7 @@ return {
 					gs.next_hunk()
 				end)
 				return "<Ignore>"
-			end, { expr = true })
+			end, { expr = true, desc = "Next Git hunk" })
 
 			map("n", "<leader>gp", gs.preview_hunk)
 			map("n", "<leader>gb", function()
@@ -45,9 +45,15 @@ return {
 
 			-- 現在の変更箇所の変更・削除を取り消して「元の状態」に復活させる (Reset Hunk)
 			map("n", "<leader>gr", gs.reset_hunk, { desc = "Reset hunk" })
+			map("v", "<leader>gr", function()
+				gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+			end, { desc = "Reset hunk (visual selection)" })
 
 			-- 現在の変更箇所だけを `git add` する (Stage Hunk)
 			map("n", "<leader>gs", gs.stage_hunk, { desc = "Stage hunk" })
+			map("v", "<leader>gs", function()
+				gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+			end, { desc = "Stage hunk (visual selection)" })
 		end,
 	},
 }
